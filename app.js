5277b03886949535ec2db15b6aa9398835a8b236
Amit Mizrahi <amitmizrahi10@gmail.com>
2014-11-05 15:27:29 -0500

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.engine('.html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(req, res) {
+	res.render('index.html');
+});
+
+app.get('/speak', function(req, res) {
+	res.render('speaker.html');
+});
+
+console.log('App listening on port ' + (process.env.PORT || 1997));
+app.listen(process.env.PORT || 1997);
